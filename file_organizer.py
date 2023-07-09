41712b7fa2dd9efcb86ecd35d5fe71cb069db8d9
Joseph <ajakayejoseph@Ajakayes-MBP.lan>
2023-07-09 21:02:52 +0100

    added argparse object to allow it recieve argument from the terminal

--- file_organizer.py
+++ file_organizer.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import shutil
 
@@ -12,8 +13,7 @@ def dir_organizer(dir_path):
     
         # check if it is a file, 
         if os.path.isfile((os.path.join(dir_path,content))): # it needs the full path of the file you want to check, if you are not in the directory where the file is located
-            file_extension = os.path.splitext(content)[1].lower()
-            print(file_extension)# split it if it is a file and get the second element
+            file_extension = os.path.splitext(content)[1].lower() # split it if it is a file and get the second element
             
             if file_extension in ['.txt', '.csv', '.log', ',py']: # check if the element extension gotten from above is in the list
                 document_folder = os.path.join(dir_path, 'document') # if it is, join the path of the directory with the string "document"
@@ -30,11 +30,24 @@ def dir_organizer(dir_path):
             elif file_extension in ['.jpg', '.jpeg', '.png', '.gif', '.bmp']:
                 image_folder = os.path.join(dir_path, 'image')
                 os.makedirs(image_folder, exist_ok=True)
-                shutil.move((os.path.join(dir_path,content)), os.path.join(dir_path, video_folder))
+                shutil.move((os.path.join(dir_path,content)), os.path.join(dir_path, image_folder))
                 
             else:
                 pass
         else:
             pass
+
+if __name__ == '__main__':
+    # Create an ArgumentParser object
+    parser = argparse.ArgumentParser(description='File Organizer Script')
+
+    # Add an argument for the directory path
+    parser.add_argument('dir_path', type=str, help='Path to the directory')
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Call the file organizer function with the provided directory path
+    dir_organizer(args.dir_path)
 
                 
